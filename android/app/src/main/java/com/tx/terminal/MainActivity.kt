4b5ba85c72a116c2b2f9f9a69034d1ba75591eaa
Rick000000007 <rickmajumdar2019@gmail.com>
2026-03-29 00:17:42 +0530

    Fix OnKeyListener import

--- android/app/src/main/java/com/tx/terminal/MainActivity.kt
+++ android/app/src/main/java/com/tx/terminal/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tx.terminal
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
